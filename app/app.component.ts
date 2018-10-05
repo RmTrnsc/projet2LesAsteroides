b5ad5d8ffb20f311349639eddabe23eaf1f1011e
RmTrnsc <tournesacromain@gmail.com>
2018-10-05 17:43:08 +0200

    base du projet

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bienvenue sur notre bibliothèque nouvelle génération';
+  title = 'titre A choisir!';
 }
