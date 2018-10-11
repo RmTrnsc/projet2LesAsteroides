aa817ea39f4bd3a1eb61b1b5c13fe92465971ccd
RmTrnsc <tournesacromain@gmail.com>
2018-10-11 15:36:28 +0200

    mise en place du routeur angular

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 import { AsteroidterreComponent } from './asteroidterre/asteroidterre.component';
 import { AsteroidceintureComponent } from './asteroidceinture/asteroidceinture.component';
 import { AsteroidconnuComponent } from './asteroidconnu/asteroidconnu.component';
@@ -9,13 +10,22 @@ import { AsteroidconnuComponent } from './asteroidconnu/asteroidconnu.component'
 @NgModule({
   declarations: [
     AppComponent,
-    AsteroidterreComponent,
     AsteroidceintureComponent,
+    AsteroidterreComponent,
     AsteroidconnuComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        { path: "Trojan asteroid", component: AsteroidceintureComponent },
+        { path: "Under approach", component: AsteroidterreComponent },
+        { path: "You know this?", component: AsteroidconnuComponent }    
+      ], 
+      
+      {useHash: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
