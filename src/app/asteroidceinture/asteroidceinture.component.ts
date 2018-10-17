f820d08bdc9d4bc17711552afde091f923947c0d
RmTrnsc <tournesacromain@gmail.com>
2018-10-17 18:10:02 +0200

    modification des carousel en fonction du device

--- src/app/asteroidceinture/asteroidceinture.component.ts
+++ src/app/asteroidceinture/asteroidceinture.component.ts
@@ -7,9 +7,72 @@ import { Component, OnInit, } from '@angular/core';
 })
 export class AsteroidceintureComponent implements OnInit {
 
-  constructor() { }
+  items; 
+    length; 
+    deg; 
+    zI; 
+    move = 0; 
+    obj;
+    yI; 
+    moove = 0;
 
-  ngOnInit() {
-  }
+    constructor(){
+        window.addEventListener('DOMContentLoaded', this.DOMContentLoaded);
+        window.addEventListener('load', this.load);
+    }
+
+
+    turn(direction) {
+
+        this.moove += direction;
+
+        for (let j = 0; j < length; j++) {
+            this.obj[j].style.transform = "rotateY(" + (this.deg * (j + this.moove)) + "deg) translateZ(" + this.yI + "px)";
+        }
+    }
+
+    DOMContentLoaded() {
+        this.items = document.getElementsByClassName('item');
+
+            length = this.items.length;
+
+        this.deg = 360 / length;
+
+            this.zI = (this.items[0].offsetWidth / 2) / Math.tan((this.deg / 2) * (Math.PI / 180))
+
+        for (let i = 0; i < length; i++) {
+            this.items[i].style.transform = "rotateY(" + (this.deg * i) + "deg) translateZ(" + this.zI + "px)";
+        }
+    }
+
+    
+
+
+    rotate(direction:any) {
+        this.move += direction;
+
+            for (let i = 0; i < length; i++) {
+                this.items[i].style.transform = "rotateY(" + (this.deg * (i + this.move)) + "deg) translateZ(" + this.zI + "px)";
+        }
+    }
+
+    load() {
+
+        this.obj = document.getElementsByClassName('item2');
+
+            length = this.obj.length;
+
+        this.deg = 360 / length;
+
+            this.yI = (this.obj[0].offsetWidth / 2) / Math.tan((this.deg / 2) * (Math.PI / 180))
+
+        for (let j = 0; j < length; j++) {
+            this.obj[j].style.transform = "rotateY(" + (this.deg * j) + "deg) translateZ(" + this.yI + "px)";
+        }
+    }
+
+    ngOnInit(){
+
+    }
 
 }
