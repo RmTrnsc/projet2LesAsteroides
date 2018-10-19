09be946ccf372d7a698db34e8a8273623e9d6e05
RmTrnsc <tournesacromain@gmail.com>
2018-10-19 09:24:02 +0200

    reponsivité

--- src/app/asteroidconnu/asteroidconnu.component.ts
+++ src/app/asteroidconnu/asteroidconnu.component.ts
@@ -15,61 +15,76 @@ export class AsteroidconnuComponent implements OnInit {
     obj;
     yI; 
     moove = 0;
-
-    constructor(){
-        window.addEventListener('DOMContentLoaded', this.DOMContentLoaded);
-        window.addEventListener('load', this.load);
-    }
-
-
-    turn(direction) {
-
-        this.moove += direction;
-
-        for (let j = 0; j < length; j++) {
-            this.obj[j].style.transform = "rotateY(" + (this.deg * (j + this.moove)) + "deg) translateZ(" + this.yI + "px)";
-        }
-    }
-
-    DOMContentLoaded() {
-        this.items = document.getElementsByClassName('item');
-
-            length = this.items.length;
-
-        this.deg = 360 / length;
-
-            this.zI = (this.items[0].offsetWidth / 2) / Math.tan((this.deg / 2) * (Math.PI / 180))
-
-        for (let i = 0; i < length; i++) {
-            this.items[i].style.transform = "rotateY(" + (this.deg * i) + "deg) translateZ(" + this.zI + "px)";
-        }
-    }
-
     
+    constructor(){
+        
+        window.addEventListener('load', this.asteroids);
+        window.addEventListener('load', this.informations);
+    }
+
+
+//------------------------------------------------|||||-ASTEROIDS-|||||-------------------------------------------------
+// --------------------------------------Function in order to do turn asteroids-----------------------------------------
+
+    turn(orientation:any) {  
+
+        this.obj = document.getElementsByClassName('item');
+        length = this.obj.length;
+        this.deg = 360 / length;
+        this.zI = (this.obj[0].offsetWidth / 2) / Math.tan((this.deg / 2) * (Math.PI / 180));
+        this.moove += orientation;
+         
+        for (let j = 0; j < length; j++) { 
+            this.obj[j].style.transform = "rotateY(" + (this.deg * (j + this.moove)) + "deg) translateZ(" + this.zI + "px)";   
+        }
+    }
+
+// --------------------------------Function in order to do a 3D effect on asteroids-------------------------------------
+
+    asteroids() {
+        this.obj = document.getElementsByClassName('item');
+        length = this.obj.length;
+        this.deg = 360 / length;
+        this.zI = (this.obj[0].offsetWidth / 2) / Math.tan((this.deg / 2) * (Math.PI / 180));
+
+        for (let k = 0; k < length; k++) {
+            this.obj[k].style.transform = "rotateY(" + (this.deg * k) + "deg) translateZ(" + this.zI + "px)";
+        }   
+    }
+
+//-------------------------------------------|||||-INFORMATIONS WINDOWS-|||||--------------------------------------------   
+//---------------------------------Function in order to do turn the informations windows --------------------------------
 
 
     rotate(direction:any) {
+
+        this.items = document.getElementsByClassName('item2');
+        length = this.items.length;
+        this.deg = 360 / length;
+        this.yI = (this.items[0].offsetWidth / 2) / Math.tan((this.deg / 2) * (Math.PI / 180));
         this.move += direction;
 
-            for (let i = 0; i < length; i++) {
-                this.items[i].style.transform = "rotateY(" + (this.deg * (i + this.move)) + "deg) translateZ(" + this.zI + "px)";
+        for (let i = 0; i < length; i++) {
+            this.items[i].style.transform = "rotateY(" + (this.deg * (i + this.move)) + "deg) translateZ(" + this.yI + "px)";
         }
     }
 
-    load() {
+//----------------------------Function in order to do a 3D effect on the informations windows----------------------------
 
-        this.obj = document.getElementsByClassName('item2');
+    informations() {
 
-            length = this.obj.length;
-
+        this.items = document.getElementsByClassName('item2');
+        length = this.items.length;
         this.deg = 360 / length;
+        this.yI = (this.items[0].offsetWidth / 2) / Math.tan((this.deg / 2) * (Math.PI / 180));
 
-            this.yI = (this.obj[0].offsetWidth / 2) / Math.tan((this.deg / 2) * (Math.PI / 180))
-
-        for (let j = 0; j < length; j++) {
-            this.obj[j].style.transform = "rotateY(" + (this.deg * j) + "deg) translateZ(" + this.yI + "px)";
+        for (let h = 0; h < length; h++) {
+            this.items[h].style.transform = "rotateY(" + (this.deg * h) + "deg) translateZ(" + this.yI + "px)";
         }
     }
+
+
+//-----------------------------------------------------------------------------------------------------------------------
 
     ngOnInit(){
 
